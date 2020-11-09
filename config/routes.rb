05bd51966cbe20ea669b75716f91bbf25665e50f
Yoel Yukalo <yoelyukalo@gmail.com>
2020-11-09 15:05:50 -0600

    finished resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :roles
+  resources :genres
+  resources :studios
+  resources :actors
+  resources :users
+  resources :reviews
+  resources :movies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
